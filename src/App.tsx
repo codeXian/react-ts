a244085a04494a8cd99d460bc066d6686fdb80e2
codexian <codexian@yeah.net>
2018-05-09 23:18:40 +0800

    test(hello component): add hello component test
    hello component name ! test

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
-import { Button } from 'antd';
+// import { Button } from 'antd';
 import * as React from 'react';
 import './App.css';
+import Hello from './components/Hello';
 
 class App extends React.Component {
   public render() {
     return (
       <div className="App">
-        <Button type="primary" >Button</Button>
+        {/* <Button type="primary" >Button</Button> */}
+        <Hello name="TypeScript" />
       </div>
     );
   }
